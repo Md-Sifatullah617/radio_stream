175fe18f14f748dbe9d606c583cc03e75e161740
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2023-06-06 22:23:06 +0600

    bottom nav

--- lib/pages/tabs/homepage.dart
+++ lib/pages/tabs/homepage.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> bottomNavPages = [
+    const Text("Home"),
+    const Text("Search"),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: bottomNavPages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xff182545),
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.black,
+        showUnselectedLabels: false,
+        currentIndex: _currentIndex,
+        items: [
+          _bottomNavItem(Icons.play_arrow, "Listen"),
+          _bottomNavItem(Icons.favorite, "Favorite")
+        ],
+        onTap: onTapEvent,
+      ),
+    );
+  }
+
+  void onTapEvent(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  BottomNavigationBarItem _bottomNavItem(IconData icons, String text) {
+    return BottomNavigationBarItem(icon: Icon(icons), label: text);
   }
 }
